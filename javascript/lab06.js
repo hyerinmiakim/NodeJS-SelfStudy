afe9ca4da757401795fd720251b2136d621e2c11
Hyerin Kim <hkim124@hawk.iit.edu>
2021-10-01 16:04:10 +0900

    [JS] 함수-3 (#lab06)
    @ Algorithm : arguments, Function 객체
    @ Date: 2021-10-01
    @ Tier: 개념

--- javascript/lab06.js
+++ javascript/lab06.js
@@ -1,5 +1,5 @@
 // ############## 클로저 ############## //
-: 내부 함수가 외부 함수의 맥락(context)에 접근할 수 있는 것*/
+// 내부 함수가 외부 함수의 맥락(context)에 접근할 수 있는 것*/
 /*function outter() {
   var title = "coding hyerin";
   function inner() {
@@ -81,3 +81,72 @@ for(var i = 0; i < 5; i++) {
 for(var index in arr) {
   console.log(arr[index]());
 }
+
+/* ########## argument ##########
+  사용자가 호출 시 넣은 인자값을 배열형태로 넣어 지원하는
+  argument 객체의 인스턴스
+*/
+/*function sun() {
+  var i, _sum = 0;
+  for(i = 0; i < arguments.length; i++) {
+    document.write(arguments[i] + <br />);
+    _sum += arguments[i];
+  }
+  return _sum;
+}
+document.write('result : ' + sum(1,2,3,4)); */
+// 함수 선언할 때 arg 갯수 명시안해주어도 호출시 넣어준 argu가 들어감
+
+/* argument.length vs 함수.length 
+  함수.length : 함수에 정의된 인자의 수
+  argument.length : 함수에 실제 전달된 인자의 수
+*/
+function zero(){
+  console.log(
+      'zero.length', zero.length,
+      'arguments', arguments.length
+  );
+} //zero.length: 0, arguments.length: 0
+function one(arg1){
+  console.log(
+      'one.length', one.length,
+      'arguments', arguments.length
+  );
+}//one.length: 1, arguments.length: 2
+function two(arg1, arg2){
+  console.log(
+      'two.length', two.length,
+      'arguments', arguments.length
+  );
+}//two.length: 2, arguments.length: 1
+zero(); // zero.length 0 arguments 0 
+one('val1', 'val2');  // one.length 1 arguments 2 
+two('val1');  // two.length 2 argumen
+// 두 값이 달라도 실행은 하지만, 이에 따라 에러 조건문 가능
+
+/* ########## 함수 호출 ########## 
+  우리가 선언하는 함수들은 Funciton이라는 객체의 인스턴스이다. 
+  그래서 Function 객체가 가지고 있는 메소드들을 상속함
+  
+  대표적인 메소드 :apply, call
+*/
+
+function sum(arg1, arg2) {
+  return arg1+ arg2;
+}
+/* arg1: 함수가 실행될 객체(null이면 window), arg2: sum에 적용할 인자들 */
+alert(sum.apply(null, [1,2]))
+
+/* Example2 */
+o1 = {val1:1, val2:2, val3:3}
+o2 = {v1:10, v2:50, v3:100, v4:25}
+function sumhl(){
+    var _sum = 0;
+    for(idx in this){  //함수를 호출했을 때 넘어온 객체
+        _sum += this[idx];
+    }
+    return _sum;
+}
+/* sumhl.apply(o1) : sumhl를 객체 o1의 메소드로 만들고, sumhl을 호출한 후에 sumhl을 삭제한다  */
+alert(sumhl.apply(o1)) // 6 : o1라는 객체에 있는 값을 더하는 것.
+alert(sumhl.apply(o2)) // 185
